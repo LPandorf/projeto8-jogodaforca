6daa24f124aa4f5e9bb52bfa30885444febc69e0
LPandorf <leticiapandorf@gmail.com>
2022-11-25 14:19:05 -0300

    teste data-test 2

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export default function Jogo(props){
-    const {imagens, erros, cor, palavraJogo, iniciarJogo, palavraEscolhida}= props;
+    const {imagens, erros, cor, palavraJogo, iniciarJogo, palavraDaRodada}= props;
     
     return(
         <Corpo>
             <Forca data-test="game-image" src={imagens[erros]} alt="imagem da forca"/>
             <BotaoEscolher data-test="choose-word" onClick={iniciarJogo}>Escolher palavra</BotaoEscolher>
-            <Palavra data-test="word"  data-answer={palavraEscolhida} cor={cor}>{palavraJogo}</Palavra>
+            <Palavra data-test="word"  data-answer={palavraDaRodada} cor={cor}>{palavraJogo}</Palavra>
         </Corpo>
     );
 }
